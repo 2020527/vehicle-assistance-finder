0798cf503840c96a384a704627571fecf698abbc
2020527 <D.Varikuti@wlv.ac.uk>
2022-05-05 16:28:44 +0100

    Login, Register API and its functionalities in frontend

---
+++ database/migrations/2022_05_04_010618_create_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id', true);
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('mechanic_id');
+            $table->string('requested_address');
+            $table->integer('requested_pincode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\UserAuthController;
+use App\Http\Controllers\MechanicsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['prefix' => 'mechanics'], function() {
+    Route::get('', [MechanicsController::class, 'index']);
+    Route::post('', [MechanicsController::class, 'store']);
+});
+
+Route::group(['prefix' => 'user'], function() {
+    Route::post('/login', [UserAuthController::class, 'login']);
+    Route::post('/register', [UserAuthController::class, 'register']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
